31dbe41d2f01e56a87767b73dbce20dc49fc5bfd
Артем Зенков <zenkov_al@skc-fmba.ru>
2016-11-03 15:52:08 +0700

    Добавил модель QmsDB

--- webreg/qmsintegration/models.py
+++ webreg/qmsintegration/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from main.models import Clinic
+
+
+class QmsDB(models.Model):
+    name = models.CharField(max_length=128, verbose_name="Название")
+    connection_param = JSONField(verbose_name="Настройки соединения")
+    db_code = models.CharField(max_length=128, verbose_name="Код основной базы данных")
+    coding = models.CharField(max_length=16, verbose_name="Кодировка базы данных")
+    clinic = models.ForeignKey(Clinic, verbose_name="Мед. учреждение")
+
+    @property
+    def settings(self):
+        return {
+            'CONNECTION_PARAM': self.connection_param,
+            'CACHE_CODING': self.coding,
+            'DATABASE_CODE': self.db_code
+        }
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "База данных QMS"
+        verbose_name_plural = "Базы данных QMS"
 
 
 class ObjectMatchingTable(models.Model):
@@ -17,3 +42,5 @@ class IdMatchingTable(models.Model):
     class Meta:
         unique_together = ('internal_id', 'object_matching_table')
         verbose_name_plural = 'Таблица соответсвий id'
+
+
