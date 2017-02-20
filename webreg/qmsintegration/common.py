c11017c375e150a4e4f6700d2f99e9e8ac59aeef
Артем Зенков <zenkov_al@skc-fmba.ru>
2017-02-20 11:40:32 +0700

    Заменил удаление и создание ячеек на обновление ячеек.

--- webreg/qmsintegration/common.py
+++ webreg/qmsintegration/common.py
@@ -18,22 +18,21 @@ def update_specialist_timetable(specialist, date_from, date_to, qms):
     qqc244 = get_external_id(specialist)
     qms_cells = qms.get_timetable(qqc244, date_from, date_to)
     # получаем все ячейки за заданный период по заданному спецу, которые лежат в базе, к которым нет назначения
-    local_cells_not_appointed = Cell.objects.filter(date__range=[date_from, date_to],date__gte=date.today(),
-                        specialist_id=specialist.id,
-                        appointment__isnull = True)
+    local_cells_not_appointed = list(Cell.objects.filter(date__range=[date_from, date_to],date__gte=date.today(),
+                                                    specialist_id=specialist.id,
+                                                    appointment__isnull = True).values_list('id', flat=True))
     # к которым eсть назначения
     local_cells_appointed = Cell.objects.filter(date__range=[date_from, date_to],
                                                 specialist_id=specialist.id,
                                                 appointment__isnull=False)
-    # удаление всех ячеек к которым не было назначений
-    local_cells_not_appointed.delete()
+
     for qms_cell in qms_cells:
         # для каждой ячейки на которую было сделано назначение удаляем из полученных,
         # так как если была запись то не редактируем
         try:
             local_cells_appointed.get(date=qms_cell.date,
-                                                       time_start=qms_cell.time_start,
-                                                       time_end=qms_cell.time_end)
+                                      time_start=qms_cell.time_start,
+                                      time_end=qms_cell.time_end)
             qms_cells.remove(qms_cell)
         except Cell.DoesNotExist:
             pass
@@ -44,21 +43,30 @@ def update_specialist_timetable(specialist, date_from, date_to, qms):
             qms_cell.time_start!=time(0,0)and \
             qms_cell.date >= date.today():
 
-            cell = Cell(
-                date=qms_cell.date,
-                specialist_id=specialist.id,
-                time_start=qms_cell.time_start, time_end=qms_cell.time_end
-            )
-            cell.slot_type = SlotType.objects.get_or_create(name=qms_cell.slot_type)[0]
+
+            slot_type = None
+            if qms_cell.slot_type:
+                slot_type = SlotType.objects.get_or_create(name=qms_cell.slot_type)[0]
+            cabinet = None
             if qms_cell.cabinet:
-                cabinet = Cabinet.objects.get_or_create(number=qms_cell.cabinet, department=specialist.department)[0]
-                if not cabinet.name:
-                    cabinet.name = "Кабинет № " + str(qms_cell.cabinet)
-                cell.cabinet = cabinet
-            cell.save()
+                cabinet = Cabinet.objects.update_or_create(defaults={'name':"Кабинет № " + str(qms_cell.cabinet)},
+                                                           number=qms_cell.cabinet,
+                                                           department=specialist.department)[0]
+
+            cell, created = Cell.objects.update_or_create(defaults={'cabinet':cabinet,
+                                                           'slot_type':slot_type},
+                                                 date=qms_cell.date,
+                                                 specialist_id=specialist.id,
+                                                 time_start=qms_cell.time_start,
+                                                 time_end=qms_cell.time_end)
+            if not created:
+                local_cells_not_appointed.remove(cell.id)
+
             for okmu in qms_cell.okmu_list:
                 cell.performing_services.add(OKMUService.objects.get_or_create(code=okmu,is_finished=1,level=5)[0])
 
+    # все оставшиеся ячейки к которым не было назначений удаляются
+    Cell.objects.filter(id__in=local_cells_not_appointed).delete()
 
 
 
