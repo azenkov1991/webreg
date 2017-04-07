c08c5d8e638cc445db76f7309f158b60469222ba
Артем Зенков <zenkov_al@skc-fmba.ru>
2017-04-07 09:26:10 +0700

    Страница подтверждения

--- webreg/patient_writer/urls.py
+++ webreg/patient_writer/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
+from patient_writer.views import PatientWriteFirstStep, PatientWriteSecondStep, Confirm
 
-from patient_writer.views import PatientWriteFirstStep, PatientWriteSecondStep
 urlpatterns = [
-    url(r'^input_first_step/$',PatientWriteFirstStep.as_view(),name="input_first_step"),
-    url(r'^input_second_step/$',PatientWriteSecondStep.as_view(),name="input_second_step"),
-    url(r'^agreement/$',TemplateView.as_view(template_name="patient_writer/agreement.html"),name="agreement")
+    url(r'^input_first_step/$', PatientWriteFirstStep.as_view(), name="input_first_step"),
+    url(r'^input_second_step/$', PatientWriteSecondStep.as_view(), name="input_second_step"),
+    url(r'^agreement/$', TemplateView.as_view(template_name="patient_writer/agreement.html"), name="agreement"),
+    url(r'^confirm/$', Confirm.as_view(), name="confirm")
 ]
--- webreg/patient_writer/views.py
+++ webreg/patient_writer/views.py
@@ -1,18 +1,36 @@
-from django.shortcuts import render
-from django.views.generic import FormView
+from django.shortcuts import redirect
+from django.views.generic import FormView, TemplateView
 from django.contrib.auth import login
 from patient_writer.forms import InputFirstStepForm, InputSecondStepForm
+from main.models import Patient
 
 class PatientWriteFirstStep(FormView):
     template_name = 'patient_writer/input_first_step.html'
     form_class = InputFirstStepForm
-    success_url = '/pwriter/input_second_step'
+    success_url = '/pwriter/confirm'
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
             login(request, form.get_user())
+        # сохранение id пациента в сессии для последующих шагов
+        request.session['patient_id'] = form.cleaned_data['patient_id']
         return super(PatientWriteFirstStep, self).post(request, *args, **kwargs)
+
+class Confirm(TemplateView):
+    template_name = 'patient_writer/confirm.html'
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        patient_id = request.session.get('patient_id',None)
+        if not patient_id:
+            return redirect("patient_writer:input_first_step")
+        try:
+            patient = Patient.objects.get(id=patient_id)
+        except Patient.DoesNotExist:
+            return redirect("patient_writer:input_first_step")
+        context['patient'] = patient
+        return self.render_to_response(context)
+
 
 class PatientWriteSecondStep(FormView):
     template_name = 'patient_writer/input_second_step.html'
