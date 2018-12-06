266da5bccf9caadc411a03d8625fe371fb5ee76b
Артем Зенков <zenkov_al@skc-fmba.ru>
2018-12-06 11:06:46 +0700

    Логи в файлы будут разбиты по директориям в зависимости от имени логгера и уровня

--- webreg/loghandle/loghandle.py
+++ webreg/loghandle/loghandle.py
@@ -32,10 +32,13 @@ class FileLogHandler(Handler):
         else:
             session_key = 'no_session_key'
 
-        if not os.path.exists(settings.LOG_DIRECTORY):
-            os.makedirs(settings.LOG_DIRECTORY)
+        log_directory_for_logger = settings.LOG_DIRECTORY + os.sep + record.name + os.sep + record.levelname
 
-        file_name = settings.LOG_DIRECTORY + os.sep + self.name + "-" + date.today().strftime("%Y%m%d")
+        if not os.path.exists(log_directory_for_logger):
+            os.makedirs(log_directory_for_logger)
+
+        file_name = log_directory_for_logger + os.sep + date.today().strftime("%Y%m%d") + ".log"
+
         with open(file_name, 'a') as f:
             f.write(datetime.now().strftime("%H:%M") + " | " + str(session_key) + " | " + record.getMessage() + "\n")
 
