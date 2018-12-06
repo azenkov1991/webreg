4bc7f51dd0b48dc29e22164a00a72d77da27b306
Артем Зенков <zenkov_al@skc-fmba.ru>
2018-12-06 09:27:55 +0700

    Логи в админке ReadOnly

--- webreg/loghandle/admin.py
+++ webreg/loghandle/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
+from mixins.admin import ReadOnlyModelAdmin
 from .models import *
 
 
-class LogModelAdmin(admin.ModelAdmin):
+class LogModelAdmin(ReadOnlyModelAdmin):
     list_display = ('date', 'time', 'session_key', 'level', 'message')
     list_filter = ('level', 'date', 'logger_name')
     search_fields = ('message', 'session_key')
 
 
-class UserActionAdmin(admin.ModelAdmin):
+class UserActionAdmin(ReadOnlyModelAdmin):
     list_filter = ('date', 'action')
     readonly_fields = ('session_key', 'user', 'patient', 'action', 'info')
     exclude = ('patient_id', 'user_id')
