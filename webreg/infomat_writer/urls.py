703140884167e2a024a1efcf9a63bfc9e2e776e1
Артем Зенков <zenkov_al@skc-fmba.ru>
2019-04-16 13:36:41 +0700

    Разрешение работать во фреймк

--- webreg/infomat_writer/urls.py
+++ webreg/infomat_writer/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.views.decorators.clickjacking import xframe_options_exempt
 from infomat_writer.views import PatientWriteFirstStep, PatientWriteSecondStep, Confirm,\
     TalonView, logout
 
@@ -8,13 +9,14 @@ from infomat_writer.views import PatientWriteFirstStep, PatientWriteSecondStep, 
 
 
 
+
 urlpatterns = [
-    url(r'^input_first_step/(?P<department>[\d+]+)/$', PatientWriteFirstStep.as_view(), name="input_first_step"),
-    url(r'^input_second_step/$', PatientWriteSecondStep.as_view(), name="input_second_step"),
-    url(r'^confirm/$', Confirm.as_view(), name="confirm"),
-    url(r'^agreement/$', TemplateView.as_view(template_name="infomat_writer/agreement.html"), name="agreement"),
-    url(r'^talon/$', TalonView.as_view(), name="talon"),
-    url(r'^accounts/logout/$', logout,
+    url(r'^input_first_step/(?P<department>[\d+]+)/$', xframe_options_exempt(PatientWriteFirstStep.as_view()), name="input_first_step"),
+    url(r'^input_second_step/$', xframe_options_exempt(PatientWriteSecondStep.as_view()), name="input_second_step"),
+    url(r'^confirm/$', xframe_options_exempt(Confirm.as_view()), name="confirm"),
+    url(r'^agreement/$', xframe_options_exempt(TemplateView.as_view(template_name="infomat_writer/agreement.html")), name="agreement"),
+    url(r'^talon/$', xframe_options_exempt(TalonView.as_view()), name="talon"),
+    url(r'^accounts/logout/$', xframe_options_exempt(logout),
         {
             'template_name': "infomat_writer/logout.html",
         },
